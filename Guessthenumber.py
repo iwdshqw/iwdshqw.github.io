ff2c1051606b3a96ec1bd7e474c7ef7c25a4b07f
iwdshqw <127856845+iwdshqw@users.noreply.github.com>
2024-03-11 15:21:01 -0500

    Add files via upload

---
+++ Guessthenumber.py
@@ -0,0 +1,14 @@
+import random
+import sleep
+Correct = 0
+Number = (random.randint(0, 10))
+Guess = input()
+
+
+if (Correct > 15):
+    print("Yes")
+else:
+   print("No")
+
+
+#Guess the number game
