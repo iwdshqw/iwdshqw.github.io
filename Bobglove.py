ebeaf7cb41f3d4588ffc807aacb7f7776f57a9f1
iwdshqw <127856845+iwdshqw@users.noreply.github.com>
2024-03-31 19:10:47 -0500

    Update Bobglove.py

--- Bobglove.py
+++ Bobglove.py
@@ -1,1 +1,13 @@
-14
+import time
+import random
+Bob = 0
+Trys = 0
+
+while Bob == 0: 
+    time.sleep(0)
+    Trys = Trys + 1
+    Number = str(random.randint(1, 2))
+    print(Number, Trys)
+    if Number == 2:
+        break
+print("finished")
