32f46eba2b9d70e4b142d3e90b8178b35e249f24
iwdshqw <127856845+iwdshqw@users.noreply.github.com>
2024-04-01 17:45:14 -0500

    Update Bobglove.py

--- Bobglove.py
+++ Bobglove.py
@@ -5,7 +5,7 @@ Trys = 0
 Time = 0
 
 while Bob == 0: 
-    time.sleep(0)
+    time.sleep(0) #sets the amount of time before looping
     Time = Time + 14
     Trys = Trys + 1
     Number = random.randint(1, 7500)
