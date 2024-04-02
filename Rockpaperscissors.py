cef8ce7628fffccfc6c1f00c03220f58440137ad
iwdshqw <127856845+iwdshqw@users.noreply.github.com>
2024-04-02 18:18:07 -0500

    Update Rockpaperscissors.py

--- Rockpaperscissors.py
+++ Rockpaperscissors.py
@@ -1,1 +1,14 @@
-r
+import time
+Choice = input("Rock, Paper or scissors: ")
+
+if Choice == "rock" or "Rock":
+    Choice = 1
+    print(Choice)
+elif Choice == "paper" or "Paper":
+    Choice = 2
+    print(Choice)
+elif Choice == "scissors" or "Scissors":
+    Choice = 3
+    print(Choice)
+else:
+    print("Say Rock, Paper or Scissors")
