f83c6613b65c04182a640fc39fc7bf10ef48b68f
iwdshqw <127856845+iwdshqw@users.noreply.github.com>
2025-02-19 10:52:42 -0600

    Update Bobglove.py
    👍

--- Bobglove.py
+++ Bobglove.py
@@ -9,7 +9,7 @@ while Bob == 0:
     Time = Time + 14
     Trys = Trys + 1
     Number = random.randint(1, 7500)
-    print(Number, Trys, Time)
+    print(Number,"Trys:",Trys,"Time(s):",Time)
     if Number == 7500:
         break
 print("finished")
